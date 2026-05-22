a576f460cc76245bc6505083ce90c55b654694d8
EulSu <146644182+GeunSuYoon@users.noreply.github.com>
2026-05-22 17:20:39 +0900

    feat: Init homework04

--- Unreal10thWeekendHomework/Unreal10thWeekendHomework/HomeworkCode/Homework04.cpp
+++ Unreal10thWeekendHomework/Unreal10thWeekendHomework/HomeworkCode/Homework04.cpp
@@ -1,13 +1,51 @@
 #include "./Homework.h"
 
-const int	KardDeckSize = 14;
-const char	KardDeck[KardDeckSize] = { 'A', '1', '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '10' , 'J' , 'Q' , 'K' };
-const char	Jocker = 'j';
+const int	ConstCardDeckSize = 14;
+const char	ConstCardDeck[ConstCardDeckSize] = { 'A', '1', '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '10' , 'J' , 'Q' , 'K' };
+const char	ConstJocker = 'j';
+const int	ConstDealerGetKard = 2;
 
 void	Homework04_Run(void);
+void	InitCardDeck(char* KardDeck);
+void	FisherYatesShuffle(int* Array, int Length);
 
 void	Homework04_Run(void)
 {
+	char	CardDeck[ConstCardDeckSize] = { 0, };
+	char	DealerCard[ConstDealerGetKard + 1] = { 0, };
 
 	printf("Homework04_Run\n");
+}
+
+void	InitCardDeck(char* KardDeck)
+{
+	for (int KardDeckCount = 0; KardDeckCount < ConstCardDeckSize; KardDeckCount++)
+	{
+		KardDeck[KardDeckCount] = ConstCardDeck[KardDeckCount];
+	}
+}
+
+void	FisherYatesShuffle(int* Array, int Length)
+{
+	//PrintArray(Array, Length);
+	//printf("\n");
+	for (int ArrayCount = 1; ArrayCount < Length; ArrayCount++)
+	{
+		int	RandomIdx = rand() % (Length - ArrayCount);
+		int	TmpVal = Array[Length - ArrayCount];
+
+		Array[Length - ArrayCount] = Array[RandomIdx];
+		Array[RandomIdx] = TmpVal;
+	}
+	//PrintArray(Array, Length);
+}
+
+void	PrintArray(int* Array, int Length)
+{
+	for (int ArrCount = 0; ArrCount < Length; ArrCount++)
+	{
+		if (ArrCount)
+			printf(", ");
+		printf("[%d]", Array[ArrCount]);
+	}
 }
